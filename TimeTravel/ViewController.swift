69c0de661dd504367c07d12962d78f1f1c528164
Ben Su <bensu1013@hotmail.com>
2016-10-04 17:50:27 -0400

    Hey again jim

--- TimeTravel/ViewController.swift
+++ TimeTravel/ViewController.swift
@@ -10,13 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var currentPlanet: UILabel!
+    @IBOutlet weak var flightStatus: UILabel!
+    
+    
+    var enterprise = Spaceship(name: "Enterprise", planet: .earth)
+    var travelToPlanet: Planet = .earth
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+    }
+    
+    @IBAction func setSpeed(_ sender: UIButton) {
+        
+        if let title = sender.titleLabel?.text {
+            switch title {
+            case "Slow":
+                enterprise.speed = .slow
+//                sender.backgroundColor = UIColor.green
+            case "Medium":
+                enterprise.speed = .medium
+//                sender.backgroundColor = UIColor.green
+            case "Fast":
+                enterprise.speed = .fast
+//                sender.backgroundColor = UIColor.green
+            case "Lightspeed":
+                enterprise.speed = .lightSpeed
+//                sender.backgroundColor = UIColor.green
+            default:
+                enterprise.speed = .none
+            }
+            
+        }
+        
+        
         
     }
-
+    
+    
+    @IBAction func setCourse(_ sender: UIButton) {
+        if let title = sender.titleLabel?.text {
+            switch title {
+            case "Mercury":
+                travelToPlanet = .mercury
+            case "Venus":
+                travelToPlanet = .venus
+            case "Earth":
+                travelToPlanet = .earth
+            case "Mars":
+               travelToPlanet = .mars
+            case "Jupiter":
+               travelToPlanet = .jupiter
+            case "Saturn":
+               travelToPlanet = .saturn
+            case "Uranus":
+                travelToPlanet = .uranus
+            case "Neptune":
+               travelToPlanet = .neptune
+            default:
+                travelToPlanet = enterprise.currentPlanet
+            }
+        }
+    }
+    
+    
+    @IBAction func launch(_ sender: UIButton) {
+        if enterprise.travel(to: travelToPlanet) {
+            flightStatus.text = "Look everybody! We made It"
+        } else {
+            flightStatus.text = "Looks like.. we got somewhere."
+        }
+        currentPlanet.text = "\(enterprise.currentPlanet)".capitalized
+    }
     
 
 
